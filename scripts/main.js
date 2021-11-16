5f9a1bb9d83c691bd3a8d5a591c97a8f7ff59c1a
rockman2021 <94093407+rockman2021@users.noreply.github.com>
2021-11-16 10:30:34 +0800

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,42 @@
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/firefox-icon.jpeg') {
+      myImage.setAttribute('src', 'images/firefox2.jpeg');
+    } else {
+      myImage.setAttribute('src', 'images/firefox-icon.jpeg');
+    }
+}
+
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+function setUserName() {
+  let myName = prompt('请输入你的名字。');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Mozilla 酷毙了，' + myName;
+}
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Mozilla 酷毙了，' + storedName;
+}
+myButton.onclick = function() {
+  setUserName();
+}
+function setUserName() {
+  let myName = prompt('请输入你的名字。');
+  if(!myName || myName === null) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML = 'Mozilla 酷毙了，' + myName;
+  }
+}
+/*
+这里的所有内容
+都是注释。
+*/
+// 这是一条注释。
